98f2129420d82561f1887582e02602bbcc657426
Stephanie Jones <stephaniejones@Stephanies-MacBook-Pro.local>
2019-01-03 13:24:38 -0700

    MessageList and Message working

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Toolbar from './Toolbar'
+import MessageList from './MessageList'
 import '../App.css';
 
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="container">
         <Toolbar />
+        <MessageList />
       </div>
     );
   }
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,1 +1,11 @@
 import React from 'react'
+import Message from './Message'
+import '../App.css'
+
+let MessageList = (props) => {
+    return (
+        <Message />
+    )
+}
+
+export default MessageList
