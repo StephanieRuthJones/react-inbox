577c2815bf176fd9a0181091415cc0d54128f821
Stephanie Jones <stephaniejones@Stephanies-MacBook-Pro.local>
2019-01-03 11:50:42 -0700

    Preliminary code in toobar.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Toolbar from './Toolbar'
+import '../App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div className="container">
+        <Toolbar />
+      </div>
     );
   }
 }
---
+++ src/components/Message.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+let Message = (props) => {
+    return (
+        
+    )
+}
